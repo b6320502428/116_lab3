3bcdf4c4646799b2698496ae835efb6757a782f1
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-15 17:05:29 +0700

    lab3_2 up3

--- lab3_2.c
+++ lab3_2.c
@@ -3,11 +3,20 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int pt[48]={0},i,j,k;
+    int pt[48]={0},i,j,k,max=0,most=0;
     for (i=0;i<n;i++)
     {
         scanf("%d %d",&j,&k);
         pt[j]=pt[j]+k;
     }
+    for (i=0;i<48;i++)
+    {
+        if(max<pt[i])
+        {
+            max=pt[i];
+            most=i;
+        }
+    }
+    printf("%d %d",most+1,max);
     return 0;
 }
