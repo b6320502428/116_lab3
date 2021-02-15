046fe5d205d2356edc2cf598253f43cade595288
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-15 18:21:23 +0700

    lab3_6 create
    ดึงcodeจากข้อ4

---
+++ lab3_6.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+int main()
+{
+    int x,p1=0,p2=0,p=0,i=2,j=2,tmp,r1=0,r2=0;
+    scanf("%d",&x);
+    while(i<=x)
+    {
+        tmp=x%i;
+        if(tmp==0)
+        {
+            break;
+        }
+        i++;
+    }
+    if(x==i)
+    {
+        p=x;
+    }
+    if(p==0)
+    {
+        for(i=x-1; i>0; i--)
+        {
+            while(j<=i)
+            {
+                tmp=i%j;
+                if(tmp==0)
+                {
+                    break;
+                }
+                j++;
+            }
+            if(i==j)
+            {
+                p1=i;
+            }
+            if(p1!=0)
+            {
+                break;
+            }
+            r1++;
+        }
+        j=2;
+        for(i=x+1; i; i++)
+        {
+            while(j<=i)
+            {
+                tmp=i%j;
+                if(tmp==0)
+                {
+                    break;
+                }
+                j++;
+            }
+            if(i==j)
+            {
+                p2=i;
+            }
+            if(p2!=0)
+            {
+                break;
+            }
+            r2++;
+        }
+        if(r2<r1)
+        {
+            p=p2;
+        }
+        else
+        {
+            p=p1;
+        }
+    }
+    printf("%d",p);
+    return 0;
+}
