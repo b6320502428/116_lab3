ecd5be69377bb925b7223f041ada31dde862e007
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-15 18:01:20 +0700

    lab3_5 up1

--- lab3_5.c
+++ lab3_5.c
@@ -3,5 +3,18 @@ int main()
 {
     int n;
     scanf("%d",&n);
+    int k,tmp=2;//ดึงจากข้อ3
+    scanf("%d",&k);
+    while(k>1)
+    {
+        while(k%tmp!=0)
+        {
+            tmp++;
+        }
+        k=k/tmp;
+        printf("%d\n",tmp);
+        tmp=2;
+    }
+    printf("0");
     return 0;
 }
