310ddaec3d3a15c6eeef8274f2e30b0e1c4b90b4
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-15 17:02:46 +0700

    lab3_2 up2

--- lab3_2.c
+++ lab3_2.c
@@ -3,10 +3,11 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int pt[48],i,j,k;
+    int pt[48]={0},i,j,k;
     for (i=0;i<n;i++)
     {
-
+        scanf("%d %d",&j,&k);
+        pt[j]=pt[j]+k;
     }
     return 0;
 }
