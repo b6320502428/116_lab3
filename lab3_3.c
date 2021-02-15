5de973c38dc76cbcc85a363869825b9a679a1aeb
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-15 17:13:21 +0700

    lab3_3 up2

--- lab3_3.c
+++ lab3_3.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 int main()
 {
-    int k;
+    int k,tmp=2;
     scanf("%d",&k);
     while(k>0)
     {
+        while(k%tmp!=0)
+        {
+            tmp++;
+        }
 
+        tmp=2;
     }
     return 0;
 }
