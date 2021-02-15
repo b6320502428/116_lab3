63b6fc680506d4ee05f075a8111770d67e78c206
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-15 18:10:59 +0700

    lab3_5 up3

--- lab3_5.c
+++ lab3_5.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    int n,tmp=2,num[3],i;
+    int n,tmp=2,num[3],i,ans;
     scanf("%d",&n);
-    for(i=0;i<3;i++)
+    int a=n;
+    for(i=0; i<3; i++)
     {
         while(n%tmp!=0)
         {
@@ -12,6 +13,10 @@ int main()
         n=n/tmp;
         num[i]=tmp;
         tmp=2;
+    }
+    if(num[0]!=num[1]&&num[0]!=num[2]&&num[1]!=num[2])
+    {
+        printf("%d is a Lucky Number.",a);
     }
     return 0;
 }
