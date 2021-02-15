ffee62bdad76aef0819e4b08487981acb43ab4f6
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-15 16:43:30 +0700

    lab3_1 up2

--- lab3_1.c
+++ lab3_1.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int score[5][4],i,j;
+    int score[5][4],i,j,st[5];
     for(i=0; i<5; i++)
     {
         for(j=0; j<4; j++)
         {
-
+            scanf("%d",&score[i][j]);
         }
     }
+
     return 0;
 }
